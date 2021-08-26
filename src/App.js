c513e1d3bcc51ddd10b379a5d4f2922d4ab0fd3a
Stephan Klinger <staeff@arcor.de>
2021-08-26 08:05:00 +0200

    Ex 2.7: Check if name exists on form submit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Person from './components/Person'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', id: 'Artos Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  // function to create new name
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      // receives content from the components newName state
+      name: newName,
+      id: newName,
+    }
+
+    const existing_ids = persons.map(person => person.id)
+
+    if (existing_ids.includes(newName)) {
+      alert(`${newName} is already added to the phonebook`)
+      setNewName('')
+    } else {
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input  type='text' value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <Person key={person.id} person={person} />
+        )}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
