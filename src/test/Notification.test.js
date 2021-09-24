a0f584eea5c0dbd264cf57e35dabfd6aa4904de4
Stephan Klinger <staeff@arcor.de>
2021-09-24 17:51:28 +0200

    Add test for notification

---
+++ src/test/Notification.test.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Notification from '../components/Notification'
+
+describe('Notification', () => {
+  let container
+
+  beforeEach(() => {
+    container = document.createElement('div')
+  })
+
+  const render = component =>
+    ReactDOM.render(component, container)
+
+  const msg = "This is a notification"
+  const null_msg = null
+  const type = "info"
+
+  it('should render a div with the right css class', () => {
+    render(<Notification msg={msg} type={type} />)
+    expect(container.querySelectorAll('div.info')).toHaveLength(1)
+  })
+
+  it('should render the correct message text', () => {
+    render(<Notification msg={msg} type={type} />)
+    expect(container.textContent).toMatch('This is a notification')
+  })
+
+  it('should not render when msg payload is null', () => {
+    render(<Notification msg={null_msg} type={type} />)
+    expect(container.querySelectorAll('div')).toHaveLength(0)
+  })
+
+})
